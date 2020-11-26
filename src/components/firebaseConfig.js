1d341085be9db01a22e3c81c2e53c8e07882ceb9
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-26 23:43:06 +0800

    firebase integ 1.1540

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default{
+  apiKey: "AIzaSyAkq26wWQf1TqRzxjUgg4dwbDSE1cj41WY",
+  authDomain: "tvs-vue-firestore.firebaseapp.com",
+  databaseURL: "https://tvs-vue-firestore.firebaseio.com",
+  projectId: "tvs-vue-firestore",
+  storageBucket: "tvs-vue-firestore.appspot.com",
+  messagingSenderId: "1095704514291",
+  appId: "1:1095704514291:web:ab34c9799b150812d57186"
+}
