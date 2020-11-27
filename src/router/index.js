5f950139604061694ae793217d9dc7633fc74af7
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-27 15:38:52 +0800

    create boilerplate vue file and other components (110) + router (408)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+//Components
+import Dashboard from '@/components/Dashboard.vue'
+import NewEmployee from '@/components/NewEmployee.vue'
+import ViewEmployee from '@/components/ViewEmployee.vue'
+import EditEmployee from '@/components/EditEmployee.vue'
+
 
 Vue.use(Router)
 
@@ -8,8 +14,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/new',
+      name: 'new-employee',
+      component: NewEmployee
+    },
+    {
+      path: '/view/:employee_id',
+      name: 'view-employee',
+      component: ViewEmployee
+    },
+    {
+      path: '/edit/:employee_id',
+      name: 'edit-employee',
+      component: EditEmployee
     }
   ]
 })
