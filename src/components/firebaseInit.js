50a44919a4b3effa3b92abc1bbebb5d0e33216a1
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-27 21:13:40 +0800

    had to retype firebaseInit and it worked now, so weird

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -1,7 +1,5 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
-
 import firebaseConfig from './firebaseConfig'
 const firebaseApp = firebase.initializeApp(firebaseConfig)
-
-export default firebaseApp.fireStore()
+export default firebaseApp.firestore()
