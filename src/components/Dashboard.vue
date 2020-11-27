14113108a5aee25353af17fa5f516c60cdcec35d
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-27 22:06:29 +0800

    get, list, and view each data at 3/7

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,11 +1,37 @@
 <template>
   <div id="dashboard">
     <h3>Dashboard</h3>
+
+    <!-- START: List Data-->
+    <ul class="collection with-header">
+      <li class="collection-header">Employees</li>
+      <li
+        class="collection-item"
+        v-for="employee in employees"
+        v-bind:key="employee.id"
+      >
+        <div class="chip">{{ employee.dept }}</div>
+        {{ employee.employee_id }}:{{ employee.name }}
+
+        <router-link
+          class="secondary-content"
+          v-bind:to="{
+            name: 'view-employee',
+            params: { employee_id: employee.employee_id },
+          }"
+          ><i class="fa fa-eye"></i>
+        </router-link>
+      </li>
+    </ul>
+    <!-- END: List Data-->
+
+    <!--START: Floating Button-->
     <div class="fixed-action-btn">
       <router-link to="/new_employee" class="btn-floating btn-large red">
         <i class="fa fa-plus"></i>
       </router-link>
     </div>
+    <!--END: Floating Button-->
   </div>
 </template>
 
@@ -21,11 +47,18 @@ export default {
 
   created() {
     db.collection("employees")
+      .orderBy("dept")
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          console.log(doc.data());
-          const data = {};
+          const data = {
+            id: doc.id,
+            employee_id: doc.data().employee_id,
+            name: doc.data().name,
+            dept: doc.data().dept,
+            position: doc.data().position,
+          };
+          this.employees.push(data);
         });
       });
   },
