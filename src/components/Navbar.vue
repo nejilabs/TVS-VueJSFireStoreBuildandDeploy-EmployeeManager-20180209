3f1d15475a24a71516b0151e79e156a1eaf25e09
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-29 01:18:02 +0800

    register create account and routing but problem with alert not displaying email. - 2157

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,12 @@
 <template>
   <nav class="nav-wrapper green">
     <div class="container">
-      <router-link to="/" class="brand-logo">Employee Manager</router-link>
+      <ul class="left">
+        <li>
+          <router-link to="/">Employee Manager</router-link>
+        </li>
+      </ul>
+
       <ul class="right">
         <li><router-link to="/">Dashboard</router-link></li>
         <li><router-link to="/login">Login</router-link></li>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -2,21 +2,24 @@
   <div>
     <div class="container">
       <div class="row">
-        <div class="col s12 m12 off-set-m2">
-          <div class="login card-panel grey lighten-4 center">
+        <div class="col s12 m8 offset-m2">
+          <div class="login card-panel grey lighten-4 black-text center">
             <h3>Register</h3>
-            <form>
+            <form action="index.html">
               <div class="input-field">
                 <i class="material-icons prefix">email</i>
-                <input type="text" id="email" />
-                <label for="email">Email</label>
+                <input type="email" id="email" v-model="email" />
+                <label for="email">Email Address</label>
               </div>
               <div class="input-field">
                 <i class="material-icons prefix">lock</i>
-                <input type="password" id="password" />
+                <input type="password" id="password" v-model="password" />
                 <label for="password">Password</label>
               </div>
-              <button class="btn btn-large grey lighten-4 black-text">
+              <button
+                v-on:click="register"
+                class="btn btn-large btn-extended grey lighten-4 black-text"
+              >
                 Register
               </button>
             </form>
@@ -28,13 +31,32 @@
 </template>
 
 <script>
+import firebase from "firebase";
 export default {
   name: "register",
-  data() {
-    return {};
+  data: function () {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    register: function (e) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(
+          (user) => {
+            // console.log(user);
+            alert(`Account Created for ${user.email}`);
+            this.$router.go({ path: this.$router.path });
+          },
+          (err) => {
+            alert(err.message);
+          }
+        );
+      e.preventDefault();
+    },
   },
 };
 </script>
-
-<style>
-</style>
