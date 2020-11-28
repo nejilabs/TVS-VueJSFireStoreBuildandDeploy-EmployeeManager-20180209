80bad1cbd053e98813471ea04a476e05e5fc8012
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-29 00:54:34 +0800

    update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,11 @@
   <nav class="nav-wrapper green">
     <div class="container">
       <router-link to="/" class="brand-logo">Employee Manager</router-link>
+      <ul class="right">
+        <li><router-link to="/">Dashboard</router-link></li>
+        <li><router-link to="/login">Login</router-link></li>
+        <li><router-link to="/register">Register</router-link></li>
+      </ul>
     </div>
   </nav>
 </template>
