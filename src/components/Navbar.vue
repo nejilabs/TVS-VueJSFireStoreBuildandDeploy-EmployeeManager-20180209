cb821d254ecfc82c9d2d9b9cdbb701d9017687c7
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-29 21:53:49 +0800

    working router guards

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,7 +11,33 @@
         <li><router-link to="/">Dashboard</router-link></li>
         <li><router-link to="/login">Login</router-link></li>
         <li><router-link to="/register">Register</router-link></li>
+        <li><button class="btn black" v-on:click="logout">Logout</button></li>
       </ul>
     </div>
   </nav>
 </template>
+
+<script>
+import firebase from "firebase";
+export default {
+  name: "navbar",
+  data() {
+    return {
+      isLoggedIn: false,
+      currentUser: false,
+    };
+  },
+
+  methods: {
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          alert("You have been logged out");
+          this.$router.push("/login");
+        });
+    },
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -10,40 +10,99 @@ import EditEmployee from '@/components/EditEmployee.vue'
 import Login from '@/components/Login.vue'
 import Register from '@/components/Register.vue'
 
+import firebase from 'firebase'
+
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
       name: 'dashboard',
-      component: Dashboard
+      component: Dashboard,
+      meta:{
+        requiresAuth:true,
+      }
     },
     {
       path: '/new_employee',
       name: 'new-employee',
-      component: NewEmployee
+      component: NewEmployee,
+      meta:{
+        requiresAuth:true,
+      }
     },
     {
       path: '/view_employee/:employee_id',
       name: 'view-employee',
-      component: ViewEmployee
+      component: ViewEmployee,
+      meta:{
+        requiresAuth:true,
+      }
     },
     {
       path: '/edit_employee/:employee_id',
       name: 'edit-employee',
-      component: EditEmployee
+      component: EditEmployee,
+      meta:{
+        requiresAuth:true,
+      }
     },
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      meta:{
+        requiresGuest:true,
+      }
     },
     {
       path: '/register',
       name: 'register',
-      component: Register
+      component: Register,
+      meta:{
+        requiresGuest:true,
+      }
     }
   ]
 })
+
+// Navbar Guards
+router.beforeEach((to,from,next) => {
+  // Check for requiredAuth guard
+  if(to.matched.some(record => record.meta.requiresAuth)){
+    // Check if user IS NOT LOGGED IN
+    if(!firebase.auth().currentUser){
+      // Got back to login
+      next({
+        path:'/login',
+        query:{
+          redirect:to.fullPath
+        }
+      })
+    }else{
+      // Proceed to route
+      next();
+    }
+  }else if(to.matched.some(record => record.meta.requiresGuest)){
+     // Check if user IS LOGGED IN
+    if(firebase.auth().currentUser){
+      // Got back to login
+      next({
+        path:'/',
+        query:{
+          redirect:to.fullPath
+        }
+      })
+    }else{
+      // Proceed to route
+      next();
+    }
+  } else{
+    // Proceed to route
+    next();
+  }
+})
+
+export default router
