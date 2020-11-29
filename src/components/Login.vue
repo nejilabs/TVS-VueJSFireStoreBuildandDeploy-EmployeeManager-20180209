6eee443873750dc16419e2639e9f74fdb24cf606
nellyXinwei <nelsonalbajr@yahoo.com>
2020-11-29 21:24:38 +0800

    login 2230

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,20 +3,23 @@
     <div class="container">
       <div class="row">
         <div class="col s12 m12">
-          <div class="login card-panel grey lighten-4 center">
+          <div class="login card-panel grey lighten-4 black-text center">
             <h3>Login</h3>
-            <form>
+            <form action="index.html">
               <div class="input-field">
                 <i class="material-icons prefix">email</i>
-                <input type="text" id="email" />
-                <label for="email">Email</label>
+                <input type="email" id="email" v-model="email" />
+                <label for="email">Email Address</label>
               </div>
               <div class="input-field">
                 <i class="material-icons prefix">lock</i>
-                <input type="password" id="password" />
+                <input type="password" id="password" v-model="password" />
                 <label for="password">Password</label>
               </div>
-              <button class="btn btn-large grey lighten-4 black-text">
+              <button
+                v-on:click="login"
+                class="btn btn-large btn-extended grey lighten-4 black-text"
+              >
                 Login
               </button>
             </form>
@@ -28,13 +31,31 @@
 </template>
 
 <script>
+import firebase from "firebase";
 export default {
   name: "login",
-  data() {
-    return {};
+  data: function () {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login: function (e) {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          (user) => {
+            alert(`You are logged in as ${user.email}`);
+            this.$router.push("/");
+          },
+          (err) => {
+            alert(err.message);
+          }
+        );
+      e.preventDefault();
+    },
   },
 };
 </script>
-
-<style>
-</style>
